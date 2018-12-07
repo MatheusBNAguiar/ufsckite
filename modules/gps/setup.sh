b9caf2ecb12621cf93755b4e464caf157865de7c
Matheus Barreto <matheus.bnaguiar@gmail.com>
2018-12-07 04:26:49 -0200

    Fix direction on file

--- modules/gps/setup.sh
+++ modules/gps/setup.sh
@@ -1,13 +1,13 @@
 wget https://www.ftdichip.com/Drivers/D2XX/Linux/libftd2xx-arm-v6-hf-1.4.8.gz
 gunzip libftd2xx-arm-v6-hf-1.4.8.gz 
 tar -xvf libftd2xx-arm-v6-hf-1.4.8
-sudo cp -r ~/release/build/lib* /usr/local/lib
+sudo cp -r ./release/build/lib* /usr/local/lib
 sudo ln -s libftd2xx.so.1.4.8 libftd2xx.so
 sudo modprobe ftdi_sio
 sudo rmmod ftdi_sio
 sudo rmmod usbserial
 
-cd ~/release/examples
+cd ./release/examples
 make
 
 bash ./pylibftdi.sh
