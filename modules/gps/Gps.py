d8dc23110e05c2f93827082186c733da4060dc65
Matheus Barreto <matheus.bnaguiar@gmail.com>
2018-12-17 02:04:32 -0200

    Fix code

--- modules/gps/Gps.py
+++ modules/gps/Gps.py
@@ -5,17 +5,20 @@ class Gps:
     def __init__(self):
         self.ser = self.init_serial()
 
+    def remove_blockage():
+        subprocess.Popen("sudo rmmod ftdi_sio", shell=True)
+        subprocess.Popen("sudo rmmod usbserial", shell=True)
+
     def init_serial(self):
+        self.remove_blockage()
         ser = serial.Serial()
         ser.baudrate = 9600
         ser.port = '/dev/ttyUSB0'
         ser.timeout = 2
         ser.open()
-
         if ser.isOpen():
             print('Open GPS serial port: ' + ser.portstr)
-        self.ser = ser
+            
     def read_data(self):
-        subprocess.Popen("sudo rmmod ftdi_sio", shell=True)
-        subprocess.Popen("sudo rmmod usbserial", shell=True)
+        self.remove_blockage()
         return self.ser.readline()
